70f3ce9b16107685d5012f2444b5aa443b707ef5
April Gilmer <aprilgilmer1@gmail.com>
2023-09-17 17:21:31 -0500

    Operators Exercise 2 Completed

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace OperatorExercise
+﻿using System;
+
+namespace OperatorExercise
 {
     public class Program
     {
@@ -12,7 +14,18 @@
             if (a == 17 && b == 4)
             {
                 Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
-            }    
+            }
+
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            double answer = AreaOfCircle(radius);
+            Console.WriteLine($"The area of a circle with radius of {radius} is {answer}");
         }
+
+        public static double AreaOfCircle(double radius)
+        {
+            double areaOfCircle = Math.PI * Math.Pow(radius, 2);            return areaOfCircle;       
+        }
+
     }
 }
